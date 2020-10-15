30fc08771c26cfa10b94d885e1087559a5eaa6cb
axxander <axander.gregory@gmail.com>
2020-10-15 12:59:11 +0100

    Tests for command-line regular expressions.

---
+++ tests/test_regex.py
@@ -0,0 +1,27 @@
+import sys 
+import os
+sys.path.append(os.path.join(os.path.realpath(os.path.dirname(__file__)), "..", "traindelay"))
+
+from constants import Constants
+import re
+import unittest
+
+
+class TestRegex(unittest.TestCase):
+
+	def test_interface(self):
+		# standard input
+		user_input = r"yrk shf 0800 2020-10-09"
+		self.assertEqual(re.search(Constants.interface_pattern, user_input).groups(), ("yrk", "shf", "0800", "2020-10-09"))
+		
+		# need to capture invalid station IDs
+		user_input = r"yrkk shf 0800 2020-10-09"
+		self.assertEqual(re.search(Constants.interface_pattern, user_input).groups(), ("rkk", "shf", "0800", "2020-10-09"))
+
+		# no match
+		user_input = r"yrkk shff 0800 2020-10-09"
+		self.assertEqual(re.search(Constants.interface_pattern, user_input), None)
+		
+
+if __name__ == '__main__':
+	unittest.main()
