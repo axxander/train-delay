2247dabae8446540c6d46131230917c2305355e0
axxander <axander.gregory@gmail.com>
2020-10-26 17:16:10 +0000

    Updated unit tests.

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -14,10 +14,6 @@ class TestRegex(unittest.TestCase):
 		user_input = r"yrk shf 0800 2020-10-09"
 		self.assertEqual(re.search(Constants.interface_pattern, user_input).groups(), ("yrk", "shf", "0800", "2020-10-09"))
 		
-		# need to capture invalid station IDs
-		user_input = r"yrkk shf 0800 2020-10-09"
-		self.assertEqual(re.search(Constants.interface_pattern, user_input).groups(), ("rkk", "shf", "0800", "2020-10-09"))
-
 		# no match
 		user_input = r"yrkk shff 0800 2020-10-09"
 		self.assertEqual(re.search(Constants.interface_pattern, user_input), None)
