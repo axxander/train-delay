d139a7f9c12ba24740e619af25469c503f9f5029
axxander <axander.gregory@gmail.com>
2020-10-27 20:14:56 +0000

    Fixed indent issues, tabs->space.

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -9,15 +9,15 @@ import unittest
 
 class TestRegex(unittest.TestCase):
 
-	def test_interface(self):
-		# standard input
-		user_input = r"yrk shf 0800 2020-10-09"
-		self.assertEqual(re.search(Constants.interface_pattern, user_input).groups(), ("yrk", "shf", "0800", "2020-10-09"))
-		
-		# no match
-		user_input = r"yrkk shff 0800 2020-10-09"
-		self.assertEqual(re.search(Constants.interface_pattern, user_input), None)
-		
+    def test_interface(self):
+        # standard input
+        user_input = r"yrk shf 0800 2020-10-09"
+        self.assertEqual(re.search(Constants.interface_pattern, user_input).groups(), ("yrk", "shf", "0800", "2020-10-09"))
+        
+        # no match
+        user_input = r"yrkk shff 0800 2020-10-09"
+        self.assertEqual(re.search(Constants.interface_pattern, user_input), None)
+        
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- traindelay/constants.py
+++ traindelay/constants.py
@@ -1,4 +1,4 @@
 class Constants:
 
-	# regex for command-line interface
-	interface_pattern = r"([a-zA-Z]{3})\s([a-zA-Z]{3})\s(\d{4})\s(\d{4}-\d{2}-\d{2})"
+    # regex for command-line interface
+    interface_pattern = r"([a-zA-Z]{3})\s([a-zA-Z]{3})\s(\d{4})\s(\d{4}-\d{2}-\d{2})"
